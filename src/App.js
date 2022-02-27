14065d863ed9b673fe59a06183f38c811ae3858b
Farhang Alizadeh <farhang.alizadeh103@gmail.com>
2022-02-27 15:32:05 +0330

    still initializing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="bg-neutral-white">Hello World</div>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center text-6xl font-manrope">
+      Hello World
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        sm: "375px",
+        lg: "1440px",
+      },
+
       colors: {
         primary: {
-          softCyan: "hsl(174, 77%, 80%)",
-          strongCyan: "hsl(174, 86%, 45%)",
-          lightGrayishRed: "hsl(14, 92%, 95%)",
-          lightRed: "hsl(15, 100%, 70%)",
-          paleBlue: "hsl(226, 100%, 87%)",
+          fullSliderBar: "hsl(174, 77%, 80%)",
+          sliderBackground: "hsl(174, 86%, 45%)",
+          discountBackground: "hsl(14, 92%, 95%)",
+          discountText: "hsl(15, 100%, 70%)",
+          ctaText: "hsl(226, 100%, 87%)",
         },
 
         neutral: {
-          white: "hsl (0, 0%, 100%)",
+          priceComponentBackground: "hsl (0, 0%, 100%)",
+          mainBackgorund: "hsl(230, 100%, 99%)",
+          emptySliderBar: "hsl(224, 65%, 95%)",
+          toggleBackground: "hsl(223, 50%, 87%)",
+          text: "hsl(225, 20%, 60%)",
+          text_ctaBackground: "hsl(227, 35%, 25%)",
         },
+      },
+
+      fontFamily: {
+        manrope: ["Manrope", ...defaultTheme.fontFamily.sans],
       },
     },
   },
