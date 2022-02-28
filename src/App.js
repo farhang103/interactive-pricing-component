8b70a01be5a94a2493a9dac3b728c3a7fdad6636
Farhang Alizadeh <farhang.alizadeh103@gmail.com>
2022-02-28 15:25:04 +0330

    styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import "./App.css";
+import bgPattern from "./assets/bg-pattern.svg";
+import patternCircles from "./assets/pattern-circles.svg";
 
 function App() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center text-6xl font-manrope">
-      Hello World
+    <div className="relative h-screen w-screen font-manrope">
+      <div className="relative flex h-1/2 w-full justify-center">
+        <img
+          src={bgPattern}
+          alt=""
+          className="absolute h-full w-full object-fill"
+        />
+        <img
+          src={patternCircles}
+          alt=""
+          className="absolute top-14 h-[9.125rem] w-[9.125rem]"
+        />
+        <h1 className="absolute top-24 text-xl font-extrabold text-neutral-text_ctaBackground">
+          Simple, traffic-based pricing
+        </h1>
+        <div className="absolute top-32 mt-1 flex flex-col items-center text-sm text-neutral-text">
+          <p>Sign-up for our 30-day trial.</p>
+          <p className="mt-2">No credit card required.</p>
+        </div>
+      </div>
+      <div className="flex w-full justify-center">
+        <div className="absolute top-[14.625rem] z-20 h-[29.9375rem] w-[20.4375rem] rounded-lg bg-neutral-priceComponentBackground">
+          <div className="flex h-[4.6875rem] w-full items-center justify-center text-sm font-extrabold tracking-widest text-neutral-text">
+            100K PAGEVIEWS
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,20 +11,20 @@ module.exports = {
 
       colors: {
         primary: {
-          fullSliderBar: "hsl(174, 77%, 80%)",
-          sliderBackground: "hsl(174, 86%, 45%)",
-          discountBackground: "hsl(14, 92%, 95%)",
-          discountText: "hsl(15, 100%, 70%)",
-          ctaText: "hsl(226, 100%, 87%)",
+          fullSliderBar: "hsl(174, 77%, 80%)", //- Soft Cyan
+          sliderBackground: "hsl(174, 86%, 45%)", //- Strong Cyan
+          discountBackground: "hsl(14, 92%, 95%)", //- Light Grayish Red
+          discountText: "hsl(15, 100%, 70%)", //- Light Red
+          ctaText: "hsl(226, 100%, 87%)", //- Pale Blue
         },
 
         neutral: {
-          priceComponentBackground: "hsl (0, 0%, 100%)",
-          mainBackgorund: "hsl(230, 100%, 99%)",
-          emptySliderBar: "hsl(224, 65%, 95%)",
-          toggleBackground: "hsl(223, 50%, 87%)",
-          text: "hsl(225, 20%, 60%)",
-          text_ctaBackground: "hsl(227, 35%, 25%)",
+          priceComponentBackground: "hsl(0, 0%, 100%)", //- White
+          mainBackgorund: "hsl(230, 100%, 99%)", //- Very Pale Blue
+          emptySliderBar: "hsl(224, 65%, 95%)", //- Light Grayish Blue
+          toggleBackground: "hsl(223, 50%, 87%)", //- Light Grayish Blue
+          text: "hsl(225, 20%, 60%)", //- Grayish Blue
+          text_ctaBackground: "hsl(227, 35%, 25%)", //- Dark Desaturated Blue
         },
       },
 
