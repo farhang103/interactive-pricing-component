341a94edca05c7ca58f7faa9708be3f27ba64997
Farhang Alizadeh <farhang.alizadeh103@gmail.com>
2022-03-02 17:20:26 +0330

    Update PriceCard.js

--- src/components/PriceCard.js
+++ src/components/PriceCard.js
@@ -5,7 +5,7 @@ import UpperPart from "./UpperPart";
 const PriceCard = () => {
   return (
     <div className="flex w-full justify-center">
-      <div className="absolute top-[14.625rem] z-20 h-[29.9375rem] w-[20.4375rem] rounded-lg bg-neutral-priceComponentBackground shadow-xl lg:h-[24.8125rem] lg:w-[33.75rem]">
+      <div className="absolute top-[14.625rem] z-20 h-[29.9375rem] w-[20.4375rem] rounded-lg bg-neutral-priceComponentBackground shadow-lg lg:h-[24.8125rem] lg:w-[33.75rem]">
         <UpperPart />
         <div className="mt-[2.375rem] h-[0.0625rem] w-full bg-[#EEF1FA]"></div>
         <LowerPart />
