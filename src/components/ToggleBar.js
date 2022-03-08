1bc382a4ff755f4a823067add9d1d8d950a61a01
Farhang Alizadeh <farhang.alizadeh103@gmail.com>
2022-03-08 14:05:44 +0330

    complete the functionality

--- src/components/ToggleBar.js
+++ src/components/ToggleBar.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ToggleBar = () => {
+const ToggleBar = ({ onClick }) => {
+  const [toggle, setToggle] = useState(true);
+
+  const toggleClass = " transform translate-x-5";
+
   return (
-    <div className="flex h-full w-11 cursor-pointer items-center rounded-full bg-neutral-toggleBackground hover:bg-[#7DE8DE]">
-      <div className="ml-1 h-[0.875rem] w-[0.875rem] rounded-full bg-white"></div>
-    </div>
+    <>
+      <div
+        className={
+          "flex h-full w-full cursor-pointer items-center rounded-full " +
+          (toggle ? "bg-neutral-toggleBackground" : "bg-[#7DE8DE]")
+        }
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        <div
+          className={
+            "ml-1 h-[0.875rem] w-[0.875rem] rounded-full bg-white transition " +
+            (toggle ? null : toggleClass)
+          }
+        ></div>
+      </div>
+    </>
   );
 };
 
--- src/components/UpperPart.js
+++ src/components/UpperPart.js
@@ -4,22 +4,29 @@ import ToggleBar from "./ToggleBar";
 
 const UpperPart = () => {
   const [value, setVal] = useState(100);
+  const [date, setDate] = useState(true);
 
   const handleChange = (e) => {
     setVal(e.target.value);
   };
 
-  let fillTrackPct = value / 2;
-  console.log(fillTrackPct);
+  const handleDate = () => {
+    setDate(!date);
+  };
+
+  const discount = date ? 0.16 : 0.12;
+
+  let fillTrack = value / 2;
 
   const style = {
     background:
       "linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) " +
-      fillTrackPct +
+      fillTrack +
       "%, hsl(224, 65%, 95%) " +
-      fillTrackPct +
+      fillTrack +
       "%, hsl(224, 65%, 95%) 100%",
   };
+
   return (
     <div>
       <div className="buble flex h-[4.6875rem] w-full items-center justify-center text-sm font-extrabold tracking-widest text-neutral-text lg:mt-5 lg:ml-11 lg:w-1/2 lg:items-center lg:justify-start lg:pt-4 lg:pl-1">
@@ -28,13 +35,16 @@ const UpperPart = () => {
       <SliderBar handleChange={handleChange} value={value} style={style} />
       <div className="flex h-16 items-center justify-center lg:absolute lg:top-8 lg:right-12">
         <p className="mr-2 text-[1.875rem] font-extrabold text-neutral-text_ctaBackground lg:text-[2.5rem]">
-          ${Math.abs(value * 0.16).toFixed(2)}
+          ${Math.abs(value * discount).toFixed(2)}
         </p>
         <p className="text-sm text-neutral-text lg:text-lg">/ month</p>
       </div>
       <div className="mt-7 flex h-[1.375rem] w-full items-center justify-evenly lg:ml-36 lg:w-[20.125rem]">
         <p className="text-xs text-neutral-text">Monthly Billing</p>
-        <ToggleBar />
+        <div className="h-full w-11" onClick={handleDate}>
+          <ToggleBar />
+        </div>
+
         <p className="text-xs text-neutral-text">Yearly Billing</p>
         <div className="flex h-full w-10 items-center justify-center rounded-full bg-primary-discountBackground">
           <p className="text-[0.625rem] font-extrabold text-primary-discountText">
